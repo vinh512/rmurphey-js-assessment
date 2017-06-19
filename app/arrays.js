8a5d577c9cd635bc15b027283442434df69b8958
vinh huynh <vvvhuynh@yahoo.com>
2017-06-18 20:27:30 -0500

    pass array methods indexOf, sum, remove

--- app/arrays.js
+++ app/arrays.js
@@ -2,15 +2,31 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.arraysAnswers = {
   indexOf: function(arr, item) {
-
+    // loop through array looking for item, once found return the index value
+    for (var i=0; i < arr.length; i++) {
+      if (arr[i] === item) {
+        return i;
+      }
+    }
+    return -1; // if not found return -1
   },
 
   sum: function(arr) {
-
+    const sum = arr.reduce(function(total, value) {
+      return total + value;
+    }, 0);
+    return sum;
   },
 
+  // iterates through array, if an item does not match push that value into a new array
   remove: function(arr, item) {
-
+    var newArr = [];
+    for (var i=0; i < arr.length; i++) {
+      if (arr[i] !== item) {
+        newArr.push(arr[i]);
+      }
+    }
+    return newArr;
   },
 
   removeWithoutCopy: function(arr, item) {
