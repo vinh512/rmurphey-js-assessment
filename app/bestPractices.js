492be2888ddb8e03f5b055f11011d41944a9fac8
vinh huynh <vvvhuynh@yahoo.com>
2017-06-19 01:45:44 -0500

    pass bestPractices

--- app/bestPractices.js
+++ app/bestPractices.js
@@ -9,18 +9,17 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.bestPracticesAnswers = {
   globals: function() {
-    myObject = {
+    var myObject = {
       name: 'Jory'
     };
-
     return myObject;
   },
 
   parseInt: function(num) {
-    return parseInt(num);
+    return parseInt(num, 10);
   },
 
   identity: function(val1, val2) {
-
+    return val1 === val2;
   }
 };
