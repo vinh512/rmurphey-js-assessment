52c84a1e46bc5a32c8063df125f59953ac9c6b13
vinh huynh <vvvhuynh@yahoo.com>
2017-06-18 22:20:00 -0500

    array methods from prepend to duplicates

--- app/arrays.js
+++ app/arrays.js
@@ -30,7 +30,7 @@ exports.arraysAnswers = {
   },
 
   removeWithoutCopy: function(arr, item) {
-    for (var i = 0, len = arr.length; i < len; i++) {
+    for (var i=0, len = arr.length; i < len; i++) {
       if (arr[i] === item) {
         arr.splice(i, 1);
         // the array has been mutated and its length decreased so the count-decrements takes that into account
@@ -52,27 +52,55 @@ exports.arraysAnswers = {
   },
 
   prepend: function(arr, item) {
-
+    arr.unshift(item);
+    return arr;
   },
 
   curtail: function(arr) {
-
+    arr.shift();
+    return arr;
   },
 
   concat: function(arr1, arr2) {
-
+    for (var i=0; i < arr2.length; i++) {
+      arr1.push(arr2[i])
+    }
+    return arr1;
   },
 
   insert: function(arr, item, index) {
-
+    arr.splice(index, 0, item);
+    return arr;
   },
 
   count: function(arr, item) {
-
+    var count = 0;
+    for (var i=0; i < arr.length; i++) {
+      if (arr[i] === item) {
+        count++;
+      }
+    }
+    return count;
   },
 
   duplicates: function(arr) {
-
+    var newArr = [];
+    var newObj = {};
+    // first iterate and keep count of hits using an object
+    for (var i=0; i < arr.length; i++) {
+      if (!newObj[arr[i]]) {
+        newObj[arr[i]] = 1;
+      } else {
+        newObj[arr[i]]++;
+      }
+    }
+    // then if there are hits, we push that value into an array
+    for (key in newObj) {
+      if (newObj[key] > 1) {
+        newArr.push(+key)
+      }
+    }
+    return newArr;
   },
 
   square: function(arr) {
